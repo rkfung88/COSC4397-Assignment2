b40bf6e4bae66dedae23f003d69c7d208d0c4506
rkfung <rkfung88@gmail.com>
2021-02-18 22:56:48 -0600

    Unity update Proj 2/18

--- Assets/RotateModel.cs
+++ Assets/RotateModel.cs
@@ -4,30 +4,52 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class ViewModel : MonoBehaviour
+public class RotateModel : MonoBehaviour
 {
     public float speed = 10f;
-    public float scale;
     public GameObject obj;
+    public GameObject obj2;
+    public GameObject curr;
+
+    [Range(0, 10)]
+    public float z = 1;
+    [Range(0, 10)]
+    public float x = 1;
+    [Range(0, 10)]
+    public float y = .2f;
+
 
     public void Slider_Change(float newSpeed)
     {
-        //Vector3 pos = obj.transform.position;
-        //pos.x = newVal;
-        //obj.transform.position = pos;
         speed = newSpeed;
-
-
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        obj.SetActive(true);
+        curr = obj;
+        obj2.SetActive(false);
+        curr.transform.Rotate(0, speed * Time.deltaTime, 0);
+        curr.transform.localScale = new Vector3(x, y, z);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        obj.transform.Rotate(0, speed * Time.deltaTime, 0);
+        //curr.transform.Rotate(0, speed * Time.deltaTime, 0);
+        //curr.transform.localScale = new Vector3(x, y, z);
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            obj.SetActive(false);
+            y = 1;
+            obj2.SetActive(true);
+            curr = obj2;
+        }
+        curr.transform.Rotate(0, speed * Time.deltaTime, 0);
+        curr.transform.localScale = new Vector3(x, y, z);
+
+
+
     }
 }
